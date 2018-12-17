68a2eb00af89f2940285d71119ab4532b2926a5b
TBleess <todd.bleess@gmail.com>
2018-12-17 11:15:03 -0700

    ACS 2017

--- Get_ACS_Tables.R
+++ Get_ACS_Tables.R
@@ -9,36 +9,42 @@ library(RPostgreSQL)
 
 geos <- c("state","county","tract","bg","place")
 state_codes <- unique(fips_codes$state_code)[1:51]
-ctys <- counties(cb = TRUE)
+#ctys <- counties(cb = TRUE)
 #states <- c("al", "ak", "az", "ar", "ca", "co", "ct", "de", "dc", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "pr", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy")
-vars2016 <- load_variables(2016, "acs5") #get the table names
-vars2016 <- vars2016[-c(1,2,3,4),] #remove extras at the top
-vars2016 <- vars2016[-c(22817:22851),] #remove extras at the bottom
-vars2016 <- vars2016[-c(21091),] #remove BLKGRP
-tablevector <- vars2016$name #create a vector of the table names
+vars2017 <- load_variables(2017, "acs5") #get the table names
+#trim unwanted table names, positions vary year to year
+# vars2011 <- vars2011[-c(42079:42101),] #remove extras at the bottom
+# vars2011 <- vars2011[-c(39732),] #remove BLKGRP
+# vars2011 <- vars2011[-c(1),] #remove extras at the top
+vars2017 <- vars2017[-c(1,2,3,4),] #remove extras at the top
+vars2017 <- vars2017[-c(25072:25106),] #remove extras at the bottom
+vars2017 <- vars2017[-c(23349),] #remove BLKGRP
+tablevector <- vars2017$name #create a vector of the table names
 tablevector <- sub("E$","",tablevector) #remove last Es
 tablevector <- sub("M$","",tablevector) #remove last Ms
 tablevector <- unique(tablevector) #ditch the duplicates
 
-# acstab <- get_acs(geography = "us", variables = c("B00001_001","B00002_001"), year = 2016, output = "wide")
+# acstab <- get_acs(geography = "us", variables = c("B00001_001","B00002_001"), year = 2011, output = "wide")
 # acstab <- rbind(acstab, get_acs(geography = "state", variables = "B00001_001", endyear = 2010, output = "wide"))
 # acstab <- rbind(acstab, get_acs(geography = "county", variables = "B00001_001", endyear = 2010, output = "wide"))
 # acstab <- rbind(acstab, get_acs(geography = "place", variables = "B00001_001", endyear = 2010, output = "wide"))
 # acstab <- rbind(acstab, get_acs(geography = "tract", state = state_codes, variables = "B00001_001", endyear = 2010, output = "wide"))
 
-#get chunks of 500 to speed things up
+# Run the following two functions as a whole to be used later
+# get chunks of 500 to speed things up
 acs500 <- function(geotype){
   x = 1
   for (i in 1:length(tablevector)){
     if ((i %% 500) == 0 | i == length(tablevector)){
       tv500 = tablevector[x:i]
+      print(tv500)
       if (x == 1){
-        acstab <- get_acs(geography = geotype, variables = tv500, year = 2016, output = "wide")
+        acstab <- get_acs(geography = geotype, variables = tv500, year = 2017, output = "wide")
       }
       else{
-        acstab <- cbind(acstab, get_acs(geography = geotype, variables = tv500, year = 2016, output = "wide"))
+        acstab <- cbind(acstab, get_acs(geography = geotype, variables = tv500, year = 2017, output = "wide"))
       }
-      print(paste(x,i))
+      #print(paste(x,i))
       x = x + 500
     }
   }
@@ -52,11 +58,11 @@ acs500tract <- function(tract_state){
     if ((i %% 500) == 0 | i == length(tablevector)){
       tv500 = tablevector[x:i]
       if (x == 1){
-        acstab <- get_acs(geography = "tract", state = tract_state, variables = tv500, year = 2016, output = "wide")
-      }
+        acstab <- get_acs(geography = "tract", state = tract_state, variables = tv500, year = 2011, output = "wide")
+      } #change geography to tract or block group depending
       else{
-        acstab <- cbind(acstab, get_acs(geography = "tract", state = tract_state, variables = tv500, year = 2016, output = "wide"))
-      }
+        acstab <- cbind(acstab, get_acs(geography = "tract", state = tract_state, variables = tv500, year = 2011, output = "wide"))
+      } #change geography to tract or block group depending
       print(paste(x,i))
       x = x + 500
     }
@@ -66,9 +72,9 @@ acs500tract <- function(tract_state){
   return(acstab)
 }
 
-#stateshort is for a partial list, change to state_codes for all states
-for (st in stateshort){
-  nam <- paste0("tract",st)
+#stateshort is for a partial list, change to state_codes for all states. Manual for block groups.
+for (st in state_codes){
+  nam <- paste0("tract",st) #change to tract or bg depending
   print(nam)
   assign(nam, acs500tract(st))
   write.csv(get(nam), file = paste0(nam,".csv"))
@@ -76,7 +82,7 @@ for (st in stateshort){
   assign(nam,NULL)
 }
 
-#Get Block Group Data - Not working well for 2010, will try again for 2016
+#Get Block Group Data - Not working well for 2010, will try again for 2011
 
 #extract fields that have block group values
 bgvector = c()
@@ -97,23 +103,23 @@ for (tv in tablevector){
 bgcolumns = read.delim("bgvector.txt",header = FALSE,sep = "\n")
 bgvector = as.vector(bgcolumns$V1)
 
-acs500bg <- function(bg_state){
-    bgtab = NULL
-    cnty_list = ctys[ctys$STATEFP == bg_state,]
-    #for (bg_county in cnty_list$COUNTYFP){
+acs500bg <- function(bg_county){
+    # bgtab = NULL
+    # cnty_list = ctys[ctys$STATEFP == bg_state,]
+    # for (bg_county in cnty_list$COUNTYFP){
       x = 1
-      #print(paste("Running",bg_county,"in",bg_state))
+      print(paste("Running",bg_county,"in",bg_state))
       for (i in 1:length(bgvector)){
-        if ((i %% 500) == 0 | i == length(bgvector)){
+        if ((i %% 100) == 0 | i == length(bgvector)){
           tv500 = bgvector[x:i]
           if (x == 1){
-            acstab <- get_acs(geography = "block group", state = bg_state, county = cnty_list$COUNTYFP, variables = tv500, endyear = 2015, output = "wide")
+            acstab <- get_acs(geography = "block group", state = bg_state, county = bg_county, variables = tv500, year = 2011, output = "wide")
           }
           else{
-            acstab <- cbind(acstab, get_acs(geography = "block group", state = bg_state, county = cnty_list$COUNTYFP, variables = tv500, endyear = 2015, output = "wide"))
+            acstab <- cbind(acstab, get_acs(geography = "block group", state = bg_state, county = bg_county, variables = tv500, year = 2011, output = "wide"))
           }
           print(paste(x,i))
-          x = x + 500
+          x = x + 100
         }
       }
     # acstab <- acstab[, !duplicated(colnames(acstab))]
@@ -125,27 +131,31 @@ acs500bg <- function(bg_state){
   #}
     #bgtab = rbind(bgtab,acstab)
   #}
+  acstab <- acstab[, !duplicated(colnames(acstab))]
+  acstab <- acstab[, order(names(acstab))]    
   return(acstab)
 }
 
-for (bg_state in stateshort){
-  #cnty_list = ctys[ctys$STATEFP == bg_state,]
-  nam <- paste0("bg",bg_state)
-  print(nam)
-  assign(nam, acs500bg(bg_state))
-  write.csv(get(nam), file = paste0(nam,".csv"))
-  print(paste0("Clearing ",nam))
-  assign(nam,NULL)
-}
+# for (bg_state in stateshort){
+#   #cnty_list = ctys[ctys$STATEFP == bg_state,]
+#   nam <- paste0("bg",bg_state)
+#   print(nam)
+#   assign(nam, acs500bg(bg_state))
+#   write.csv(get(nam), file = paste0(nam,".csv"))
+#   print(paste0("Clearing ",nam))
+#   assign(nam,NULL)
+# }
 
 
 
 
 
-#Table Manipulation
-getdata <- function(file)
-states <- read.csv(file="states.csv", header=TRUE, sep=",", colClasses = "character")
+#Table Manipulation to add us and states
+#getdata <- function(file)
+#states <- read.csv(file="states.csv", header=TRUE, sep=",", colClasses = "character")
+#states <- rbind(acstabus, acstabstate) #cbind the results of acs500("us") and acs500("states")
 
+states <- states[, order(names(states))]
 #Split data and margin of error
 statedata <- select_(states, lazyeval::interp(~ends_with(x), x = "E"))
 statedata$NAM <- NULL
@@ -167,9 +177,9 @@ statemoe <-cbind(NAME=states$NAME,statemoe)
 statedata$GEONUM <- sub("^","1",statedata$GEONUM)
 statemoe$GEONUM <- sub("^","1",statemoe$GEONUM)
 
-#Brute force the US Geonum
-statedata[1,2] <- "30"
-statemoe[1,2] <- "30"
+#Brute force the US Geonum (only use for US level data)
+#statedata[1,2] <- "30"
+#statemoe[1,2] <- "30"
 
 #make all columns numeric
 statedata <- sapply(statedata, as.numeric) #drop Name and Nam in production
@@ -177,7 +187,7 @@ statemoe <- sapply(statemoe, as.numeric)
 
 #Connect to Postgresql
 pg = dbDriver("PostgreSQL")
-con = dbConnect(pg, user="postgres", password="password", host="104.197.26.248", port=5433, dbname="acs1216")
+con = dbConnect(pg, user="postgres", password="password", host="104.197.26.248", port=5433, dbname="acs0711")
 
 testcolumns <- colnames(statemoe) #change to statemoe to load moe
 
@@ -206,6 +216,7 @@ for (column in testcolumns){
 }
 
 #append to already written tables
+testcolumns <- colnames(statedata) #change to statemoe to load moe
 temptable <- "temptable"
 tabname <- ""
 collist <- c()
@@ -271,7 +282,7 @@ getdata <- function(file,type){
 
 #Connect to Postgresql
 pg = dbDriver("PostgreSQL")
-con = dbConnect(pg, user="postgres", password="password", host="104.197.26.248", port=5433, dbname="acs0610")
+con = dbConnect(pg, user="postgres", password="", host="104.197.26.248", port=5433, dbname="acs0711")
 temp = list.files(pattern="*.csv")
 
 #loop for tract files
@@ -310,3 +321,111 @@ for (tractfile in temp){
   }
   print(paste0(tractfile," is complete"))
 }
+
+#place loading loop
+#x <- c("001","002","003","004","005","006","007","008","009")
+#for (val in x){
+temp = list.files(pattern="*.csv")
+
+#loop for tract files
+for (tractfile in temp){ 
+  print(paste0(tractfile, " has begun"))
+  states <- read.csv((file=tractfile), header=TRUE, sep=",", colClasses = "character")
+  #overnight loop for places
+  states <- states[, order(names(states))]
+  statedata <- select_(states, lazyeval::interp(~ends_with(x), x = "E"))
+  statedata$NAM <- NULL
+  statemoe <- select_(states, lazyeval::interp(~ends_with(x), x = "M"))
+  
+  #Rename fields
+  names(statedata) <- substr(names(statedata),1,nchar(names(statedata))-1)
+  names(statedata) <- sub("_","",names(statedata))
+  names(statemoe) <- substr(names(statemoe),1,nchar(names(statemoe))-1)
+  names(statemoe) <- sub("_","_MOE",names(statemoe))
+  
+  #Add name and geoid columns
+  statedata <-cbind(GEONUM=as.character(states$GEOID),statedata)
+  statedata <-cbind(NAME=states$NAME,statedata)
+  statemoe <-cbind(GEONUM=as.character(states$GEOID),statemoe)
+  statemoe <-cbind(NAME=states$NAME,statemoe)
+  
+  #Change geoid to geonum
+  statedata$GEONUM <- sub("^","1",statedata$GEONUM)
+  statemoe$GEONUM <- sub("^","1",statemoe$GEONUM)
+  
+  #make all columns numeric
+  statedata <- sapply(statedata, as.numeric) #drop Name and Nam in production
+  statemoe <- sapply(statemoe, as.numeric)
+  
+  testcolumns <- colnames(statedata) #change to statemoe to load moe
+  temptable <- "temptable"
+  tabname <- ""
+  collist <- c()
+  for (column in testcolumns){
+    if (column == "NAME" | column == "GEONUM" | column == "NAM"){}
+    else{
+      colstart <- substr(column, start = 1, stop = nchar(column)-3)
+      #print(colstart)
+      if (colstart != tabname){
+        if (is.null(collist) == FALSE){
+          dbtable <- as.data.frame(subset(statedata,select=c("GEONUM",collist))) #change to moe if necessary
+          #dbtable <- as.data.frame(sapply(dbtable, as.numeric)) Only necessary when as.numeric above fails 
+          names(dbtable) <- tolower(names(dbtable))
+          dbWriteTable(con,c('data',temptable),dbtable,row.names=FALSE)
+          sql <- paste0("INSERT INTO data.", tolower(tabname)," SELECT * FROM data.", temptable,";" )
+          dbSendQuery(con, sql)
+          dropsql <- paste0("DROP TABLE data.", temptable)
+          dbSendQuery(con,dropsql)
+        }
+        #print(collist)
+        collist <- c(column)
+        tabname <- colstart
+      }
+      else {
+        collist <- c(collist,column)
+      }
+    }
+  }
+  
+  testcolumns <- colnames(statemoe) #change to statemoe to load moe
+  temptable <- "temptable"
+  tabname <- ""
+  collist <- c()
+  for (column in testcolumns){
+    if (column == "NAME" | column == "GEONUM" | column == "NAM"){}
+    else{
+      colstart <- substr(column, start = 1, stop = nchar(column)-3)
+      #print(colstart)
+      if (colstart != tabname){
+        if (is.null(collist) == FALSE){
+          dbtable <- as.data.frame(subset(statemoe,select=c("GEONUM",collist))) #change to moe if necessary
+          #dbtable <- as.data.frame(sapply(dbtable, as.numeric)) Only necessary when as.numeric above fails 
+          names(dbtable) <- tolower(names(dbtable))
+          dbWriteTable(con,c('data',temptable),dbtable,row.names=FALSE)
+          sql <- paste0("INSERT INTO data.", tolower(tabname)," SELECT * FROM data.", temptable,";" )
+          dbSendQuery(con, sql)
+          dropsql <- paste0("DROP TABLE data.", temptable)
+          dbSendQuery(con,dropsql)
+        }
+        #print(collist)
+        collist <- c(column)
+        tabname <- colstart
+      }
+      else {
+        collist <- c(collist,column)
+      }
+    }
+  }
+  print(tractfile)
+}
+
+#loop to run block groups by county in colorado
+counties = c("001","003","005","007","009","011","013","014","015","017","019","021","023","025","027","029","031","033","035","037","039","041","043","045","047","049","051","053","055","057","059","061","063","065","067","069","071","073","075","077","079","081","083","085","087","089","091","093","095","097","099","101","103","105","107","109","111","113","115","117","119","121","123","125")
+for (cnty in counties){
+  nam <- paste0("bg08",cnty)
+  assign(nam, acs500bg(cnty))
+  write.csv(get(nam), file = paste0(nam,".csv"))
+  print(paste0("Clearing ",nam))
+  assign(nam,NULL)
+}
+
